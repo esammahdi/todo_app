bcbfb8a3b810f1e6459ec9f4a9ecc437b2125f73
esammahdi <95491137+esammahdi@users.noreply.github.com>
2022-04-25 13:48:34 +0300

    change initial state of the object to 'RegisteringServiceState' and add event handlers for 'LoginEvent', 'RegisteringServiceEvent'

--- lib/home/bloc/homebloc_bloc.dart
+++ lib/home/bloc/homebloc_bloc.dart
@@ -1,14 +1,33 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app/services/todo.dart';
+
+import '../../services/authentication.dart';
 
 part 'homebloc_event.dart';
 part 'homebloc_state.dart';
 
 class HomeblocBloc extends Bloc<HomeblocEvent, HomeblocState> {
-  HomeblocBloc() : super(HomeblocInitial()) {
-    on<LoginEvent>(  (event,emitter) {
-      
+  final AuthenticationService _auth;
+  final TodoService _todo;
+
+  HomeblocBloc(this._auth, this._todo) : super(const RegisteringServicesState()) {
+    on<LoginEvent>(  (event,emit) async{
+      final reply = await _auth.authenticateUser(event.username, event.password);
+      if(reply == event.username) {
+        emit(SuccessfulLoginState(event.username));
+        emit(HomeblocInitial());
+        print('Successful Login');
+      } else {
+        print('Failed Login');
       }
+      } 
     );
+
+    on<RegisteringServicesEvent>((event, emit) async{
+        await _auth.init();
+        await _todo.init();
+        emit(HomeblocInitial());
+      });
   }
 }
