efdb02b16b59df0cf5bdc81c8ea5c86a4bced9ff
esammahdi <95491137+esammahdi@users.noreply.github.com>
2022-04-25 13:41:25 +0300

    add some users manually for testing purposes (to be removed later)

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -1,21 +1,24 @@
-
 import 'package:hive/hive.dart';
 import 'package:todo_app/model/user.dart';
 
 class AuthenticationService {
-
   late Box<User> _users;
-  Future<void> int() async {
+  Future<void> init() async {
     Hive.registerAdapter(UserAdapter());
     _users = await Hive.openBox<User>('usersBox');
+    await _users.clear();
+    await _users.add(User('Esam Mahdi', '1234'));
+    await _users.add(User('Mahdi', '1234'));
+    await _users.add(User('', ''));
   }
 
-  Future<String?> authenticateUser(final String username,final String passwordHash) async {
-    final success = _users.values.any((element) => element.username == username && element.passwordHash == passwordHash);
-    if(success) {
+  Future<String?> authenticateUser(
+      final String username, final String passwordHash) async {
+    final success = _users.values.any((element) =>
+        element.username == username && element.passwordHash == passwordHash);
+    if (success) {
       return username;
-    }
-    else {
+    } else {
       return null;
     }
   }
