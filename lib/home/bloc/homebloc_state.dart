a2497930fe3f34a355d9cff55a813157a4cb238f
esammahdi <95491137+esammahdi@users.noreply.github.com>
2022-04-25 13:50:19 +0300

    add 'RegisteringServiceState' and 'SuccefulLoginState'

--- lib/home/bloc/homebloc_state.dart
+++ lib/home/bloc/homebloc_state.dart
@@ -2,14 +2,27 @@ part of 'homebloc_bloc.dart';
 
 abstract class HomeblocState extends Equatable {
   const HomeblocState();
-  
 }
 
-class HomeblocInitial extends HomeblocState {
-
-  final bool? success;
-
-  HomeblocInitial({this.success});
+class HomeblocInitial extends HomeblocState {  
+  const HomeblocInitial();
     @override
-  List<Object?> get props => [success];
+  List<Object?> get props => [];
+}
+
+class SuccessfulLoginState extends HomeblocState {
+
+  final String username;
+
+  const SuccessfulLoginState(this.username);
+    @override
+  List<Object?> get props => [username];
+}
+
+class RegisteringServicesState extends HomeblocState {
+
+  const RegisteringServicesState();
+
+  @override
+  List<Object?> get props => [];
 }
